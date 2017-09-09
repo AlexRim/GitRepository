9a1f2e64b7cd03776f5d4a1f352e013baeb2031b
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-09 11:33:26 +0300

    Start TestGitProject

---
+++ Application/Application/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Application
+{
+    class Program
+    {
+        private static string Print() => "Hello World!";
+
+        static void Main(string[] args)
+        {
+
+            Write(Print());
+            ReadKey();
+        }
+    }
+}
