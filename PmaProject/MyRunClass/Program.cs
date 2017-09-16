3abf5b7f97d852ab880933d95372e3b10295543c
Alexander Rimasheuski <Squire_86@mail.ru>
2017-09-16 19:31:18 +0300

    New

---
+++ PmaProject/MyRunClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+
+
+namespace MyRunClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"e:\GeckoDriver");
+            // service.FirefoxBinaryPath = @"c:\Program Files\Mozilla Firefox\firefox.exe";
+            //var driver = new FirefoxDriver(service);
+
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://localhost/pma");
+ 
+         
+
+          
+
+            driver.Close();
+        }
+    }
+}
--- PmaProject/PmaProject/Class1.cs
+++ PmaProject/PmaProject/Class1.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
 
 namespace PmaProject
 {
+    [TestFixture]
     public class Class1
     {
+        private IWebDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"e:\GeckoDriver");
+            service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+           driver = new FirefoxDriver(service);
+        }
+
+        [Test]
+        public void Navigate()
+        {
+            driver.Navigate().GoToUrl("http://www.google.com");
+            var wait = new WebDriverWait(driver, TimeSpan.FromMinutes(1));
+            var clickableElement = wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.Id("hplogo")));
+            string title= driver.FindElement(By.Id("hplogo")).GetAttribute("title");
+            Assert.AreEqual("Google", title);
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+
+
+
+
     }
 }
